217fa0752bdffc52e9d477e584cc3ea4a6c34c53
KachaPooja <malvipoo2205@gmail.com>
2023-07-06 18:43:41 +0530

    function types

--- module-5/Module-5/JavaScript/Events/2.Mouse Event/js/sqrt.js
+++ module-5/Module-5/JavaScript/Events/2.Mouse Event/js/sqrt.js
@@ -1,7 +1,7 @@
 function squrt()
 {
     var a = document.getElementById("ename").value;
-    var sq = document.write(Math.sqrt(a));
+    var sq = Math.sqrt(a);
     document.getElementById("sqr").value=sq;
 
 }
