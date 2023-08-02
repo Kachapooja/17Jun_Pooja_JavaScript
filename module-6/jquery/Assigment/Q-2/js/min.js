bb9534030cfb93e04c9311f56e142adc30e1be27
KachaPooja <malvipoo2205@gmail.com>
2023-08-02 18:23:43 +0530

    jquery data uploaded

---
+++ module-6/jquery/Assigment/Q-2/js/min.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//     $("#btnadd").click(function(){
+        
+//         $("p").addClass("red");
+//     });
+
+//     $("#btnrmv").click(function(){
+        
+//         $("p").removeClass("red");
+//     });
+// });
