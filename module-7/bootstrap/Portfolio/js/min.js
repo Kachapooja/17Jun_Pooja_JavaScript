99f81543e5ab1f4f058b83048235f6911b0eb9c1
KachaPooja <malvipoo2205@gmail.com>
2023-08-28 01:16:09 +0530

    bootstrao data

---
+++ module-7/bootstrap/Portfolio/js/min.js
@@ -0,0 +1,9 @@
+{/* <script>
+    var typed = new Typed(".chng", {
+        strings: ["Kacha Pooja", 
+        "Web Developer","Web Designer"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true,
+    });
+</script> */}
